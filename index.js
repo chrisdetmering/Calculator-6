4aadd72cc819eac87e3859b8e1e27753816f9421
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-05-20 21:45:15 -0700

    Day 2

--- index.js
+++ index.js
@@ -1,159 +1,222 @@
-let zero = document.querySelector('.zero');
-let one = document.querySelector('.one');
-let two = document.querySelector('.two');
-let three = document.querySelector('.three');
-let four = document.querySelector('.four');
-let five = document.querySelector('.five');
-let six = document.querySelector('.six');
-let seven = document.querySelector('.seven');
-let eight = document.querySelector('.eight');
-let nine = document.querySelector('.nine');
-let period = document.querySelector('.dot');
-let equal = document.querySelector('.equal');
-
-let clear = document.querySelector('.clear');
-let multiply = document.querySelector('.multiply');
-let divide = document.querySelector('.divide');
-let add = document.querySelector('.add');
-let subtract = document.querySelector('.subtract');
 let outPut = document.querySelector('.output');
+let btnArr = Array.from(document.getElementsByClassName('btn'));
 
 let arr = [];
+let memory = [];
 let change;
 let total = 0;
 let dotControl = true;
-let equalControl;
+let equalControl = 4;
+let memoryControl = true;
 
-zero.addEventListener('click', () => {
-    if(arr != '') {
-        arr.push(0);
-        converter();
 
-    } else {
-    outPut.innerText = 0;
-    }
-})
+addEventListener('click', (e) => {
+    let item = e.target;
 
-one.addEventListener('click', () => {
-    arr.push(1);
-    converter();
-})
+    let find = btnArr.indexOf(item);
+   
+    switch(find){
+        case 0:
+            if(memoryControl == false){
+                arr = [];
+                arr.push(memory[0]);
+                converter();
+            } else {
+                outPut.innerText = '0';
+                arr = [];
+                equalControl = 4;
+            } 
+            break;
+        case 5:
+            if(outPut.innerText != 0){
+                memory.shift();
+                memory.push(outPut.innerText); 
+                memoryControl = false;
+            } 
+            
+            break;  
+        case 10:
+            memory.shift();
+            memoryControl = true;
+            break;    
+        //memory
 
-two.addEventListener('click', () => {
-    arr.push(2);
-    converter();
-})
+        case 1:
+            if(arr.length < 20) {
+            arr.push(7);
+            converter();
+            }
+            break;
+        case 2:
+            if(arr.length < 20) {
+            arr.push(4);
+            converter();
+            }
+            break;
+        case 3:
+            if(arr.length < 20) {
+            arr.push(1);
+            converter();
+            }
+            break;
+        case 4:
 
-three.addEventListener('click', () => {
-    arr.push(3);
-    converter();
-})
+            if(arr != '' && arr.length < 20) {
+                arr.push(0);
+                converter();
+            } else if(arr == ''){
+            outPut.innerText = 0;
+            }
 
-four.addEventListener('click', () => {
-    arr.push(4);
-    converter();
-})
+            break;
+        case 6:
+            if(arr.length < 20) {
+            arr.push(8);
+            converter();
+            }
+            break;
+        case 7:
+            if(arr.length < 20) {
+            arr.push(5);
+            converter();
+            }
+            break;
+        case 8:
+            if(arr.length < 20) {
+            arr.push(2);
+            converter();
+            }
+            break;
+        case 9:
+            if(arr.length < 20) {
+                if(dotControl == true) {
+                    if(arr == '') {
+                        arr.push(0);
+                    }
+                    arr.push('.');
+                    dotControl = false;
+                    converter();
+                }
+                
+            } 
+            break;
+        case 11:
+            if(arr.length <20) {
+            arr.push(9);
+            converter();
+            }
+            break;
+        case 12:
+            if(arr.length < 20) {
+            arr.push(6);
+            converter();
+            }
+            break;
+        case 13:
+            if(arr.length < 20) {
+            arr.push(3);
+            converter();
+            }
+            break;
+        //0-9 and .
+        
+        case 14:
 
-five.addEventListener('click', () => {
-    arr.push(5);
-    converter();
-})
+            calculate();
+            //equal
+            break;
+        case 15:
 
-six.addEventListener('click', () => {
-    arr.push(6);
-    converter();
-})
+            arr = [];
+            change;
+            total = 0;
+            dotControl = true;
+            outPut.innerText = 0;
+            equalControl = 4;
+            //clear
+            break;
+        case 16:
+            if(arr !=''){
+                if(equalControl == 2) {
+                    calculate();
+                } else {
+                    calculate();
+                    equalControl = 2;
+                }
+            }
+            //divide
+            break;
+        case 17:
+            if(arr != ''){
+                if(equalControl == 3) {
+                    calculate();
+                } else {
+                    calculate();
+                    equalControl = 3;
+                }
+            }
+            //multi
+            break;
+        case 18:
+            if(arr != '') {
+                if(equalControl == 1) {
+                    calculate();
+                } else {
+                    calculate();
+                    equalControl = 1;
+                }
+            }
+            //subtract
+            break;
+        case 19:
+            if(arr != '') {
+                if(equalControl == 0) {
+                    calculate();
+                } else {
+                    calculate();
+                    equalControl = 0;
+                }
+            }
+            //add
+            break;
+        // add/sub/div/multi/
 
-seven.addEventListener('click', () => {
-    arr.push(7);
-    converter();
-})
-
-eight.addEventListener('click', () => {
-    arr.push(8);
-    converter();
-})
-
-nine.addEventListener('click', () => {
-    arr.push(9);
-    converter();
-})
-
-period.addEventListener('click', () => {
-
-    if(dotControl == true) {
-        if(arr == '') {
-            arr.push(0);
         }
-        arr.push('.');
-        dotControl = false;
-    }
-    
-    converter();
-})
+
+}) 
 
 function converter() {
     change = arr.join('');
     outPut.innerText = change;
-}
+} 
 
-add.addEventListener('click', () => {
-    equalControl = 0;
-    master();
-})
+function calculate() {
 
-subtract.addEventListener('click', () => {
-    equalControl = 1;
-    master();
-})
-
-divide.addEventListener('click', () => {
-    equalControl = 2;
-    master();
-})
-
-multiply.addEventListener('click', () => {
-    equalControl = 3;
-    master();
-})
-
-clear.addEventListener('click', () => {
-    arr = [];
-    change;
-    total = 0;
-    dotControl = true;
-    outPut.innerText = 0;
-    equalControl;
-})
-
-equal.addEventListener('click', () => {
-    master();
-    equalControl = 4;
-})
-
-function master() {
-    if(total == 0) {
-        total = +change;
-    } else {
     switch(equalControl){
         case 0:
-            total += +change;
+            total += parseFloat(change);
             break;
         case 1:
-            total -= +change;
+            total -= parseFloat(change);
             break;
         case 2:
-            total /= +change;
+            total /= parseFloat(change);
             break;
         case 3:
-            total *= +change;
+            total *= parseFloat(change);
             break;
         case 4:
-            total = +total;
+            if(change == undefined) {
+                total = 0;
+            } else if(total == 0) {
+                total = parseFloat(change);
+            } else {
+                total;
+            }
             break;
-        }
     }
+    
     outPut.innerText = total;
     arr = [];
     dotControl = true;
-}
+} 
